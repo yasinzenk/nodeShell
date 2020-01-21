9bd0eccfdafbca8160abdcf80e9270168be16203
Yan <ing.yangao@gmail.com>
2020-01-21 12:13:34 -0500

    we did pwd

---
+++ bash.js
@@ -0,0 +1,19 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if(cmd === "pwd"){
+    process.stdout.write(`${process.cwd()}`);
+  }
+  else{
+    process.stdout.write('You typed: ' + cmd);
+  }
+  process.stdout.write('\npromt > ');
+});
+
+// process.stdin.on('data', (data) => {
+//   const cmd = data.toString().trim();
+
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\npromt > ');
+// });
